1a65bcfc2ba8b89dbabb9daf6b95faee59504e56
brash85 <126812304+brash85@users.noreply.github.com>
2023-05-23 14:28:09 +0000

    connect

--- script.js
+++ script.js
@@ -17,17 +17,42 @@ function shirts() {
 }
 shirts();
 
-function colors() {
-  var color = ["RED", "TAN", "WHITE", "BLACK", ];
-  var option = document.createElement("option");
-  option.innerHTML = (color[0]);
-  var dropbox = document.getElementById("color-select");
-  dropbox.appendChild(option);
-  for (var i = 1; i < color.length; i++) {
-    option = document.createElement("option");
-    option.innerHTML = (color[i]);
-    dropbox.append(option);
+const ptoducts = [
+  { id: "product1", name: "RED", price: 10.00},
+  {id: "product2", name: "TAN", price: 10.00},
+  {id: "product3", name: "RED", price: 10.00},
+  {id: "product4", name: "RED", price: 10.00}
+];
+let cartItems = [];
+let cartTotal = 0;
+
+function addItem(){
+const selectElement = document.getElementById("color-select");
+const selectedProductId = selectElement.value;
+
+if (selectedProductId){
+  const selectedProduct = products.find((product) => product.id === selectedProductId);
+
+  if (selectedProduct) {
+    cartItems.push(selectedProduct);
+    cartTotal += selectedProduct.price;
+    updateCart();
   }
+}
+};
+
+function updateCart(){
+  const cartItemsElement = document.getElementById("cart-items");
+  const cartTotalElement = document.getElementById("cart-total");
+
+  cartItemsElement.innerHTML = "";
+
+
+  cartItems.forEach((item) => {
+    const li = document.createElement("li");
+    li.textContent = '${item.name} - $${item.price.toFixed(2)}';
+    cartItemsElement.appendChild(li);
+  });
+  
 
 }
-colors();
